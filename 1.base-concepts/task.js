7f0c0b32f812a7ef5822ef53429e124fc51767c5
MaksimNosov <m.s.nosov@gmail.com>
2022-11-29 01:07:18 +0300

    1.base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,39 +1,29 @@
 'use strict';
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = Math.pow(b, 2) - 4 * a * c;
-  if (d < 0) {
-    arr = [];
-  }
+    let arr = [];
+    let d = Math.pow(b, 2) - 4 * a * c;
 
-  if (d === 0) {
-    arr = [(b * (-1)) / ( 2 * a)];
-  }
+    if (d === 0) {
+        arr = [(b * (-1)) / (2 * a)];
+    }
 
-  if (d > 0) {
-    arr = [((b * (-1)) + Math.sqrt(d) ) / (2 * a), ((b * (-1)) - Math.sqrt(d) ) / (2 * a)];
-  }
-  
-  return arr;
+    if (d > 0) {
+        arr = [((b * (-1)) + Math.sqrt(d)) / (2 * a), ((b * (-1)) - Math.sqrt(d)) / (2 * a)];
+    }
+
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  if (isNaN(percent)) {
-    return false;
-  }
-  
-  if (isNaN(contribution)) {
-    return false;
-  }
-  
-  if (isNaN(amount)) {
-    return false;
-  }
+    if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
+        return false;
+    }
 
-  let convertedPercent = percent / 100;
-  let monthlyPercent = convertedPercent / 12;
-  let loanBody = amount - contribution;
-  let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / (Math.pow((1 + monthlyPercent), countMonths) - 1)));
-  let totalPaymentAmount = Number((monthlyPayment * countMonths).toFixed(2));
-  return totalPaymentAmount;
+    let convertedPercent = percent / 100;
+    let monthlyPercent = convertedPercent / 12;
+    let loanBody = amount - contribution;
+    let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / (Math.pow((1 + monthlyPercent), countMonths) - 1)));
+    let totalPaymentAmount = Number((monthlyPayment * countMonths).toFixed(2));
+    return totalPaymentAmount;
 }
